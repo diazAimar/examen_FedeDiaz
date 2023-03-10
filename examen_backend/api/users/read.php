caaad21a5d8626275769cb6ad856ee4b3d3fa7d0
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-03-10 15:22:31 -0300

    ✨ Create User model and read api endpoint

---
+++ examen_backend/api/users/read.php
@@ -0,0 +1,35 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once('../../Database.php');
+include_once('../../Models/User.php');
+
+$database = new Database();
+$db = $database->connect();
+$params = $_POST["params"];
+$user = new User($db);
+$res = $user->list($params);
+$num = $res->rowCount();
+
+if ($num > 0) {
+  $users_arr = array();
+  $users_arr["data"] = array();
+  while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
+    $user_item = array(
+      'id' => $id,
+      'name' => $name,
+      'dni' => $dni,
+      'gender' => $gender,
+      'age' => $age,
+    );
+    array_push($users_arr["data"], $user_item);
+  }
+  echo json_encode($users_arr);
+} else {
+  echo json_encode(
+    array("message" => 'No Users Found')
+  );
+}
