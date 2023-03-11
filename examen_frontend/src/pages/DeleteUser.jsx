362875cfd7afb9dbdc9b1cdcfd0b661267cd3b1c
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-03-11 14:05:06 -0300

    ✨ Add create, delete, and edit user pages.

---
+++ examen_frontend/src/pages/DeleteUser.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+import axios from 'axios';
+
+import { toast } from 'react-toastify';
+
+import apiInstance from '../instance';
+
+export default function DeleteUser() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserById = async () => {
+      let result = null;
+      result = await apiInstance.get(`/users/read_single_by_id.php?uid=${params.uid}`);
+      if (result?.data.error === false) {
+        setUser(result.data.user);
+        setIsLoading(false);
+      } else {
+        console.log(result.data.message);
+        navigate('/users');
+      }
+    };
+    fetchUserById();
+  }, []);
+
+  const deleteUser = async () => {
+    console.log('fire delete');
+    const result = await apiInstance.delete(
+      'http://localhost/examen_backend/api/users/delete.php',
+      {
+        data: {
+          id: user.id,
+        },
+      }
+    );
+    if (result?.data.error === false) {
+      toast.success(result.data.message + ' Navigating to dashboard.');
+      setTimeout(() => {
+        navigate('/users');
+      }, 1500);
+    } else if (result?.data.error === true) {
+      toast.error(result.data.message);
+    }
+  };
+
+  return (
+    <div className="">
+      {!isLoading && user !== null ? (
+        <div className="w-1/3 mx-auto">
+          <h2 className="py-2 px-4 bg-red-700 text-[2rem] text-white font-bold rounded-t-[.5rem]">
+            Delete Confirmation
+          </h2>
+          <div className="bg-red-50">
+            <div className="p-4 flex flex-col gap-2">
+              <p>You are about to delete the user:</p>
+              <Link to={`/users/edit/${user.id}`} className="link text-blue-500">
+                {user.name} {user.surname} (id {user.id})
+              </Link>
+              <p>All asociated data will also be deleted.</p>
+              <p>
+                Are you sure? <span className="font-bold"> You can't undo this.</span>
+              </p>
+              <button
+                className="btn btn-error w-1/2 capitalize"
+                onClick={() => {
+                  deleteUser();
+                }}
+              >
+                Delete User
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
