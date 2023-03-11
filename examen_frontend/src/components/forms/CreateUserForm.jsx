a257e08f2e54a7a8413a27ddf9ab1bada88f97c4
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-03-11 19:43:22 -0300

    ✨ Fix frontend validations

--- examen_frontend/src/components/forms/CreateUserForm.jsx
+++ examen_frontend/src/components/forms/CreateUserForm.jsx
@@ -13,13 +13,19 @@ export default function CreateUserForm() {
   const createUserSchema = Yup.object().shape({
     name: Yup.string().trim().max(255, 'Too long!').required('Required'),
     surname: Yup.string().trim().max(255, 'Too long!').required('Required'),
-    dni: Yup.number().max(99999999, 'Too long!').required('Required'),
+    dni: Yup.number()
+      .min(1, 'Must be between 1 and 99999999')
+      .max(99999999, 'Must be between 1 and 99999999')
+      .required('Required'),
     gender: Yup.string()
       .trim()
       .max(1, 'Too long!')
       .matches(/^[FM]$/, 'Must be F (Female) or M (Male)')
       .required('Required'),
-    age: Yup.number().max(199, 'Too long!').required('Required'),
+    age: Yup.number()
+      .min(1, 'Must be between 1 and 199')
+      .max(199, 'Must be between 1 and 199')
+      .required('Required'),
   });
 
   const inputsArray = [
