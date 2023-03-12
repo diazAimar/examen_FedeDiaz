61cb3a42497353c2d32aed78b9535e5eca60efe0
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-03-12 16:15:40 -0300

    ✨ Add DeleteCourse and EditCourse pages

--- examen_frontend/src/components/shared/CourseCard.jsx
+++ examen_frontend/src/components/shared/CourseCard.jsx
@@ -1,8 +1,19 @@
+import { Link } from 'react-router-dom';
+
 export default function CourseCard({ course }) {
   return (
     <div className="w-full border-2 border-t-0 border-lapis-lazuli rounded-[.5rem]">
-      <div className="text-primary-white bg-lapis-lazuli border-0 py-2 px-4 text-[rem] rounded-t-[.5rem] z-10">
+      <div className="text-primary-white bg-lapis-lazuli border-0 py-2 px-4 text-[rem] rounded-t-[.5rem] z-10 flex justify-between">
         {course.modality_description}
+        <div>
+          <Link to={`/courses/edit/${course.id}`} className="text-right">
+            📝
+          </Link>
+
+          <Link to={`/courses/delete/${course.id}`} className="text-right">
+            ❌
+          </Link>
+        </div>
       </div>
       <ul>
         <li>
--- examen_frontend/src/pages/courses/EditCourse.jsx
+++ examen_frontend/src/pages/courses/EditCourse.jsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+
+import { useParams, useNavigate } from 'react-router-dom';
+
+import EditCourseForm from '../../components/forms/EditCourseForm';
+
+import apiInstance from '../../instance';
 
 export default function EditCourse() {
-  return <div>EditCourse</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [course, setCourse] = useState(null);
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchCourseById = async () => {
+      let result = null;
+      result = await apiInstance.get(`/courses/read_single_by_id.php?cid=${params.cid}`);
+      if (result?.data.error === false) {
+        setCourse(result.data.course);
+        setIsLoading(false);
+      } else {
+        navigate('/Courses');
+      }
+    };
+    fetchCourseById();
+  }, []);
+
+  return (
+    <div className="">
+      {!isLoading && course !== null ? (
+        <div>
+          <h2 className="mb-4 text-center">Edit Course</h2>
+          <EditCourseForm course={course} />
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
