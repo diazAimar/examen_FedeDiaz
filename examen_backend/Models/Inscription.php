0bc122541d03ed0c4697bad443e4c08d931e22fa
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-03-13 02:08:32 -0300

    ✨ readSingleById and delete Inscription funcions

--- examen_backend/Models/Inscription.php
+++ examen_backend/Models/Inscription.php
@@ -25,6 +25,7 @@ class Inscription
   public function read()
   {
     $select_query = 'SELECT
+                      i.id,
                       i.user_id, 
                       u.name as user_name,
                       u.surname as user_surname,
@@ -39,13 +40,34 @@ class Inscription
                     LEFT JOIN
                       courses c ON i.course_id = c.id
                     ORDER BY
-                      c.created_at DESC';
+                      i.created_at DESC';
 
     $stmt = $this->conn->prepare($select_query);
 
     $stmt->execute();
 
     return $stmt;
+  }
+
+  /* Read single inscription by id */
+  public function readSingleById()
+  {
+    $select_query = "SELECT * FROM " . $this->table . " WHERE id = :id";
+    $select_stmt = $this->conn->prepare($select_query);
+    $select_stmt->bindParam(':id', $this->id);
+    $select_stmt->execute();
+
+    $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$row) {
+      return false;
+    }
+    $this->course_id = $row['course_id'];
+    $this->user_id = $row['user_id'];
+
+    if ($select_stmt->rowCount() > 0) {
+      return true;
+    }
+    return false;
   }
 
   /* Create inscription */
@@ -59,6 +81,21 @@ class Inscription
     $insert_stmt->bindParam(':course_id', $this->course_id);
     $insert_stmt->bindParam(':user_id', $this->user_id);
     if ($insert_stmt->execute()) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /* Delete inscription */
+  public function delete()
+  {
+    $delete_query = "DELETE FROM " . $this->table . ' WHERE id=:id';
+
+    $delete_stmt = $this->conn->prepare($delete_query);
+    $delete_stmt->bindParam(':id', $this->id);
+
+    if ($delete_stmt->execute()) {
       return true;
     }
 
