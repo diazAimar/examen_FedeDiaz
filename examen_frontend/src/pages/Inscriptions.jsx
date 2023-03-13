2c70e5a2141ef1f93e4d7701d4f373e5dc807acb
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-03-13 02:08:42 -0300

    ✨ Inscription "home" page finished

--- examen_frontend/src/pages/Inscriptions.jsx
+++ examen_frontend/src/pages/Inscriptions.jsx
@@ -1,12 +1,51 @@
+import { useState, useEffect } from 'react';
+
 import { Link } from 'react-router-dom';
 
+import apiInstance from '../instance';
+
 export default function Inscriptions() {
+  const [isLoading, setIsLoading] = useState(null);
+  const [inscriptions, setInscriptions] = useState(null);
+
+  useEffect(() => {
+    const fetchInscriptions = async () => {
+      let result = null;
+      result = await apiInstance.get('/inscriptions/read.php');
+      const inscriptions = result.data.inscriptions;
+      setInscriptions(inscriptions);
+      setIsLoading(false);
+    };
+    fetchInscriptions();
+  }, []);
+
   return (
     <div className="">
       <div className="mb-8">
         <Link to="/inscriptions/create" className="btn capitalize">
           Enroll User to Course
         </Link>
+        {isLoading ? (
+          <p>Loading</p>
+        ) : inscriptions ? (
+          <div className="mt-5">
+            <h3>Inscriptions:</h3>
+            <div className="flex flex-col gap-y-8">
+              {inscriptions.map((inscription) => {
+                return (
+                  <p key={inscription.id} className="text-[1.2rem]">
+                    {inscription.course_name}{' '}
+                    <Link to={`/inscriptions/delete/${inscription.id}`} className="mt-2 inline">
+                      ❌
+                    </Link>
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <p>No inscriptions</p>
+        )}
       </div>
     </div>
   );
