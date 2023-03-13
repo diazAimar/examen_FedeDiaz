17c721c17cb80e75f62d79a51b8e2dc6d9e48041
diazAimar <federico.diaz@est.fi.uncoma.edu.ar>
2023-03-13 12:29:24 -0300

    ✨ Send inscriptions to course card

--- examen_frontend/src/pages/Courses.jsx
+++ examen_frontend/src/pages/Courses.jsx
@@ -11,6 +11,7 @@ export default function Courses() {
   const [isLoading, setIsLoading] = useState(true);
   const [individualCourses, setIndividualCourses] = useState(null);
   const [groupCourses, setGroupCourses] = useState(null);
+  const [inscriptions, setInscriptions] = useState(null);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -26,6 +27,19 @@ export default function Courses() {
       setGroupCourses(gCourses);
       setIsLoading(false);
     };
+
+    const fetchInscriptions = async () => {
+      const fetchInscriptions = async () => {
+        let result = null;
+        result = await apiInstance.get('/inscriptions/read.php');
+        const inscriptions = result.data.inscriptions;
+        setInscriptions(inscriptions);
+        setIsLoading(false);
+      };
+      fetchInscriptions();
+    };
+
+    fetchInscriptions();
     fetchUsers();
   }, []);
 
@@ -41,14 +55,20 @@ export default function Courses() {
       ) : (
         <div className="flex justify-around">
           <div className="flex flex-col gap-y-8">
-            {individualCourses.map((course) => {
-              return <CourseCard course={course} key={course.id} />;
-            })}
+            {individualCourses?.length > 0 &&
+              individualCourses.map((course) => {
+                return (
+                  <div key={course.id}>
+                    <CourseCard course={course} inscriptions={inscriptions} />
+                  </div>
+                );
+              })}
           </div>
           <div className="flex flex-col gap-y-8">
-            {groupCourses.map((course) => {
-              return <CourseCard course={course} key={course.id} />;
-            })}
+            {groupCourses?.length > 0 &&
+              groupCourses.map((course) => {
+                return <CourseCard course={course} key={course.id} inscriptions={inscriptions} />;
+              })}
           </div>
         </div>
       )}
